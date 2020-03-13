e9a818386bc9dd14fa991518310718b1fec6738e
Angrycranberie <mathdep38@sfr.fr>
2020-03-13 13:39:40 +0100

    algo Jarvis

--- src/AlgoJarvis.java
+++ src/AlgoJarvis.java
@@ -9,6 +9,7 @@ public class AlgoJarvis {
     public static void main(Fenetre f, String [] args) {
         Random r = new Random();
         List<Point> l = new LinkedList<Point>();
+        List<Point> ec = new LinkedList<Point>();
         int nbPoints;
 
         // Reccuperation du nombre de points en argument (ou valeur par defaut)
@@ -27,6 +28,41 @@ public class AlgoJarvis {
             l.add(p);
             f.tracerSansDelai(p);
         }
+
+
+        //Algo Jarvis
+        Point pini = l.get(0), pcourant, pprec, psuiv = null;
+        for(Point p : l){
+            if(pini.x > p.x){
+                pini = p;
+            }
+        }
+        ec.add(pini);
+        pcourant = pini;
+        pprec = new Point(pini.x -1, pini.y);
+
+        do{
+            int x1, x2, y1, y2;
+            double Aire = 0;
+            for(Point p : l){
+
+                if(p != pprec && p != pcourant){
+                    x1 = pcourant.x - pprec.x;
+                    y1 = pcourant.y - pprec.y;
+                    x2 = p.x - pcourant.x;
+                    y2 = p.y - pcourant.y;
+                    if(Aire < x1*y2-x2*y1){
+                        Aire = x1*y2-x2*y1;
+                        psuiv = p;
+                    }
+                }
+            }
+            pprec = pcourant;
+            pcourant = psuiv;
+            ec.add(pcourant);
+
+
+        }while (pcourant != pini);
 
 
 
