0268d9c4e997ec6ad1ac8848fd2c6a243ed798e2
Angrycranberie <mathdep38@sfr.fr>
2020-03-13 16:46:10 +0100

    zerthj

--- src/AlgoJarvis.java
+++ src/AlgoJarvis.java
@@ -6,10 +6,12 @@ import java.util.*;
 
 
 public class AlgoJarvis {
-    static int Orientation(Point p, Point q, Point pj){
-        int val = (q.y - p.y) * (pj.x - q.x) - (q.x - p.x) * (pj.y - q.y);
-        if(val == 0) return 0;
-        return (val > 0)? 1:2;
+
+    //Fonction qui nous donne le sens de parcours : sens horaire ou anti-horaire
+    static int sens(Point pcourant, Point pj, Point psuiv){
+        int s = (psuiv.y - pcourant.y) * (pj.x - psuiv.x) - (psuiv.x - pcourant.x) * (pj.y - psuiv.y);
+        if(s == 0) return 0;
+        return (s > 0)? 1:2;
     }
 
     public static void main(Fenetre f, String [] args) {
@@ -34,55 +36,56 @@ public class AlgoJarvis {
             f.tracerSansDelai(p);
         }
 
+        //Test avec un nuage de point carré
+        /*for(int i = 1; i<6;i++ ){
+            for(int j = 1; j<6;j++){
+                Point p = new Point(i*50,j*50);
+                l.add(p);
+                f.tracerSansDelai(p);
+            }
+        }*/
+
 
         //Algo Jarvis
-        Point pini = l.get(0), pcourant, pprec,q;
+        Point pini = l.get(0), pcourant,psuiv;
+
+        //On récupère le point le plus à gauche
         for(Point p : l){
             if(pini.x > p.x){
                 pini = p;
             }
         }
+        //Initialisation
         pcourant = pini;
-       // pprec = new Point(pini.x -1, pini.y);
-        // f.tracer(new Segment(pcourant.x, pcourant.y,pprec.x, pprec.y));
+
         do{
-            //int x1, x2, y1, y2;
-            //double angle = -2;
+            //On ajoute le point courant à l'enveloppe convexe
             ec.add(pcourant);
 
-            q = l.get((l.indexOf(pcourant)+1)%l.size());
+            //Calcul du point suivant par rapport au point courant de tel sort que sens(pcourant,pj,psuiv) soit
+            // anti-horaire. On cherche le point avec l'angle le plus grand (dans le sens anti-horaire)
+            psuiv = l.get((l.indexOf(pcourant)+1)%l.size());
+
             for(Point p : l){
 
-                /*if(p != pprec && p != pcourant && !ec.contains(p) ){
-                    f.tracer(new Segment(pcourant.x, pcourant.y,p.x,p.y,Color.red));
-                    x1 = pcourant.x - pprec.x;
-                    y1 = pcourant.y - pprec.y;
-                    x2 = p.x - pcourant.x;
-                    y2 = p.y - pcourant.y;
-                    double angletemp = Math.acos((x1*y1 + x2*y2)/(Math.sqrt(x1*x1 + y1*y1)*Math.sqrt(x2*x2 + y2*y2)));
-
-                    if(angle < angletemp){
-                        angle = angletemp;
-                        psuiv = p;
-                    }
-                }*/
+                //On affiche le segment que l'on test
                 f.tracer(new Segment(pcourant.x, pcourant.y,p.x,p.y,Color.red));
 
-                if(Orientation(pcourant, q, p)==2){
-                    q = p;
+                //Si le point p donne un meilleur résultat, on met à jour le point suicant
+                if(sens(pcourant, p, psuiv)==2){
+                    psuiv = p;
                 }
 
                 f.effacer(new Segment(pcourant.x, pcourant.y,p.x,p.y));
             }
-            f.tracer(new Segment(pcourant.x, pcourant.y, q.x,q.y));
-            pcourant = q;
+            //On efface le segment testé
+            f.tracer(new Segment(pcourant.x, pcourant.y, psuiv.x,psuiv.y));
 
+            //On met à jour le point courant
+            pcourant = psuiv;
 
+            //Tant qu'on n'est pas revenu au début
         }while (pcourant != pini);
-
-
-
-
 
     }
 }
